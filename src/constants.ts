b520165ad4d05425226779622de5503f04e85444
techpixel <yralqfwus@mozmail.com>
2024-04-05 21:04:54 +0000

    set channel output to prod

--- src/constants.ts
+++ src/constants.ts
@@ -32,8 +32,8 @@ export type HackHourSession = {
 // Constants
 export const Constants = {
 //HACK_HOUR_CHANNEL: 'C06T6MQ1AMN', // PRIVATE
-//HACK_HOUR_CHANNEL: '           ', // PROD
-  HACK_HOUR_CHANNEL: 'C06S6E7CXK7', // DEV
+  HACK_HOUR_CHANNEL: 'C06SBHMQU8G', // PROD
+//HACK_HOUR_CHANNEL: 'C06S6E7CXK7', // DEV
   MIN_MS: 60 * 1000,
   HOUR_MS: 60 * 60 * 1000,
 
