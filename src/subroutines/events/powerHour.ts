01718ffccb892b76c7f1813a40d39ab5c2985591
techpixel <yralqfwus@mozmail.com>
2024-04-28 13:55:17 -0500

    adjust hours to something a little more realistic and fair

--- src/subroutines/events/powerHour.ts
+++ src/subroutines/events/powerHour.ts
@@ -17,7 +17,7 @@ class PowerHour implements BasePicnic {
     ID = POWERHOUR_ID;
 
     START_TIME = new Date("2024-04-27T06:00:00-0500");
-    END_TIME = new Date("2024-05-04T12:00:00-0500");
+    END_TIME = new Date("2024-05-04T23:59:00-0500");
 
     CUSTOM_NAME = "powerhour.exe";
     CUSTOM_EMOJI = ":hourglass_flowing_sand:";
