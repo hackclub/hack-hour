edb6ba60378a3640e602848c302c3bc22c135506
techpixel <68567672+techpixel@users.noreply.github.com>
2024-05-28 01:29:55 -0500

    scrap arcade in favor of shipper (of yay)

---
+++ src/subroutines/ship/index.ts
@@ -0,0 +1,5 @@
+import { emitter } from "../../lib/emitter.js";
+import { app } from "../../lib/bolt.js";
+import { prisma } from "../../lib/prisma.js";
+import { formatHour } from "../slack/lib/templates.js";
+
