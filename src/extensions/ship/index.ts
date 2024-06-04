bbe4d8d934454eaa2d4104f2da8f967c00edf015
techpixel <68567672+techpixel@users.noreply.github.com>
2024-06-04 18:15:32 -0400

    actually update sessions this time

--- src/extensions/ship/index.ts
+++ src/extensions/ship/index.ts
@@ -666,7 +666,27 @@ app.command(Commands.SESSIONS, async ({ command, ack }) => {
             });
             continue;
         }
-        if (!(session.metadata as any).airtable.status) {
+        if (!(session.metadata as any).airtable.status || (session.metadata as any).airtable.status === "Manual/Status Unavailable") {
+            // Fetch the status from Airtable
+            const airtableSession = await AirtableAPI.Session.fetch((session.metadata as any).airtable.id);
+
+            if (!airtableSession) {
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `*${session.createdAt.getMonth()}/${session.createdAt.getDate()}*\n${(session.metadata as any).work}\n_Goal:_ ${session.goal?.name}\n*Not submitted*: Please send a message in <#C06U5U9ADGD>\n<${(await app.client.chat.getPermalink({
+                            channel: Environment.MAIN_CHANNEL,
+                            message_ts: session.messageTs
+                        })).permalink
+                            }|View Session>`
+                    }
+                }, {
+                    "type": "divider"
+                });
+                continue;
+            }
+
             session = await prisma.session.update({
                 where: {
                     messageTs: session.messageTs
@@ -676,8 +696,8 @@ app.command(Commands.SESSIONS, async ({ command, ack }) => {
                         ...(session.metadata as any),
                         airtable: {
                             ...(session.metadata as any).airtable,
-                            status: "Manual/Status Unavailable",
-                            reason: null
+                            status: airtableSession.fields["Status"],
+                            reason: airtableSession.fields["Reason"]
                         }
                     }
                 },
