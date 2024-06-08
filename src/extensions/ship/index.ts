452db6bd6385cee61e0b50fb1b1cf307d8192377
techpixel <68567672+techpixel@users.noreply.github.com>
2024-06-07 23:19:46 -0400

    stringify

--- src/extensions/ship/index.ts
+++ src/extensions/ship/index.ts
@@ -188,7 +188,7 @@ app.action(Actions.OPEN_SESSION_REVIEW, async ({ ack, body }) => {
         await ack();
 
         const blocks = await Ship.openSessionReview(user.id, shipTs);
-        emitter.emit('debug', `DEBUGAHH\n${blocks}`)
+        emitter.emit('debug', `DEBUGAHH\n${JSON.stringify(blocks)}`)
 
         await app.client.chat.update({
             channel: id,
@@ -203,7 +203,7 @@ app.action(Actions.OPEN_SESSION_REVIEW, async ({ ack, body }) => {
         });
     } catch (error) {
         emitter.emit('error', error);
-        emitter.emit('debug', `DEBUGAHH\n${body}`)
+        emitter.emit('debug', `DEBUGAHH\n${JSON.stringify(body)}`)
     }
 });
 
