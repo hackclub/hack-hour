8650df6ebe7b4c7bf1a414f6a01ecbfd05870a66
techpixel <68567672+techpixel@users.noreply.github.com>
2024-06-12 20:27:35 -0400

    sync dev

---
+++ src/extensions/arcade/slack/views/scrapbook_flow.ts
@@ -0,0 +1,88 @@
+import { Actions, Callbacks } from "../../../../lib/constants.js";
+import type { Session } from "@prisma/client";
+
+export function chooseSessionsButton(slackId: string, scrapbookId: string) {
+    return [
+        {
+            type: "section",
+            text: {
+                type: "plain_text",
+                text: "Select which sessions should be linked to your scrapbook post!",
+                emoji: true,
+            },
+        },
+        {
+            type: "actions",
+            block_id: "actions",
+            elements: [
+                {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "Choose Sessions",
+                        emoji: true,
+                    },
+                    value: JSON.stringify({ slackId, scrapbookId }),
+                    action_id: Actions.CHOOSE_SESSIONS,
+                },
+            ],
+        },
+    ];
+}
+
+export function chooseSessionsModal(sessions: Session[], scrapbookId: string) {
+    return {
+        type: "modal" as const,
+	    callback_id: Callbacks.CHOOSE_SESSIONS,
+        private_metadata: scrapbookId,
+        title: {
+            type: "plain_text" as const,
+            text: "Choose Sessions",
+            emoji: true,
+        },
+        submit: {
+            type: "plain_text" as const,
+            text: "Submit",
+            emoji: true,
+        },
+        close: {
+            type: "plain_text" as const,
+            text: "Cancel",
+            emoji: true,
+        },
+        blocks: [
+            {
+                type: "section",
+                text: {
+                    type: "plain_text",
+                    text: "Choose one or more sessions that are associated with your scrapbook post!",
+                    emoji: true,
+                },
+            },
+            {
+                type: "divider",
+            },
+            {
+                type: "input",
+                block_id: "sessions",
+                element: {
+                    type: "checkboxes",
+                    options: sessions.map(session => ({
+                        text: {
+                            type: "mrkdwn",
+                            text: `**${session.metadata.work}** - ${session.createdAt.toDateString()}`,
+                            emoji: true,
+                        },
+                        value: session.metadata.airtable?.id!,
+                    })),
+                    action_id: "sessions",
+                },
+                label: {
+                    type: "plain_text",
+                    text: "Unlinked Sessions",
+                    emoji: true,
+                },
+            },
+        ],
+    };
+}
