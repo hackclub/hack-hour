76ee270a6494f873a3758450c528083cb16dc4fe
techpixel <68567672+techpixel@users.noreply.github.com>
2024-06-14 09:55:12 -0400

    use firstime instead of airtable

--- src/extensions/arcade/index.ts
+++ src/extensions/arcade/index.ts
@@ -1,5 +1,5 @@
 import "./watchers/scrapbook.js";
-import "./watchers/hackhour.js";
+import "./watchers/hackhour.js"
 import "./watchers/airtable.js";
 import "./slack/index.js";
 import "./slack/shop.js";
