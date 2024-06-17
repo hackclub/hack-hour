55cdf36e4fe6059d3308c31e578ad7baa3e10535
Max Wofford <max@maxwofford.com>
2024-06-17 13:06:42 -0400

    Json.stringify

--- src/extensions/slack/lib/lib.ts
+++ src/extensions/slack/lib/lib.ts
@@ -38,7 +38,7 @@ export async function updateTopLevel(session: Session) {
         return;
     }
     const blocks = await TopLevel.topLevel(session);
-    console.log("🔥🔥🔥🔥", blocks)
+    console.log("🔥🔥🔥🔥", JSON.stringify(blocks, null, 2))
     await Slack.chat.update({
         ts: session.messageTs,
         channel: Environment.MAIN_CHANNEL,
