a52d230b0d201125ed930ffddcb81cb0fa48109f
Max Wofford <max@maxwofford.com>
2024-06-26 15:07:30 -0400

    Add background job for scrapbook review

--- src/extensions/arcade/watchers/arcade_review.ts
+++ src/extensions/arcade/watchers/arcade_review.ts
@@ -17,15 +17,58 @@ const getArcadeScrapbooksToReview = async () => {
     return records
 }
 
+const getArcadeScrapbooksToApprove = async () => {
+    const filterRules = [
+        '{Count Unreviewed Sessions} = 0',
+        'Approved != TRUE()',
+        `RECORD_ID() = 'recKjFPT8CMeZV3F2'` // test record
+    ]
+    // TODO: also include re-reviews in this list
+    const filter = `AND(${filterRules.join(', ')})`
+    const records = await AirtableAPI.Scrapbook.filter(filter)
+    return records
+}
+
+const getArcadeScrapbooksToGarbageCollect = async () => {
+    const filterRules = [
+        'NOT(Approved = TRUE())',
+        'NOT(BLANK() = {Review TS})',
+        `RECORD_ID() = 'recKjFPT8CMeZV3F2'` // test record
+    ]
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const main = async () => {
-    console.log('Checking for arcade scrapbooks to review...')
+    const reviewJob = async () => {
+        try {
+            const scrapbooks = await getArcadeScrapbooksToReview();
+            const scrapbook = scrapbooks[0];
+            await Review.init(scrapbook.id);
+        } catch(e) {
+            console.error(e);
+        }
+        await sleep(1000 * 10); // wait 10 seconds
+        return reviewJob() // run again
+    }
 
-    const scrapbooks = await getArcadeScrapbooksToReview();
+    const approveJob = async () => {
+        try {
+            const scrapbooks = await getArcadeScrapbooksToApprove();
+            for (const scrapbook of scrapbooks) {
+                await Review.approveScrapbook(scrapbook.id);
+            }
+        } catch(e) {
+            console.error(e)
+        }
+        await sleep(1000 * 5); // wait 5 seconds
+        return approveJob() // run again
+    }
 
-    console.log(scrapbooks);
+    const garbageCollectionJob = async () => {
 
-    for (const scrapbook of scrapbooks) {
-        await Review.init(scrapbook.id);
     }
 }
 
