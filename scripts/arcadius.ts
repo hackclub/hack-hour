ff3ed4847d69023f053ce7657fdaa6c5e21be163
techpixel <68567672+techpixel@users.noreply.github.com>
2024-06-30 15:49:13 -0400

    cleanup

--- scripts/arcadius.ts
+++ scripts/arcadius.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import Airtable from "airtable";
 
 const hoursAirtable = new Airtable({
-    apiKey: 'patVUxTHohsrJmX02.30383e201931e539a475f80cc11bd3b6d3176fdb5901f97baffc88a275d06893'
+    apiKey: '',
 }).base("app4kCWulfB02bV8Q")("Users");
 
 hoursAirtable.create({
