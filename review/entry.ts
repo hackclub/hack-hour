71a796687abd5fef3757b560380263c06fe8bc28
techpixel <68567672+techpixel@users.noreply.github.com>
2024-07-12 16:39:01 -0400

    run review & hackhour in parallel

--- review/entry.ts
+++ review/entry.ts
@@ -7,9 +7,9 @@ import main from './poll';
 try {
     await app.start(process.env.REVIEW_PORT || 5876);
 
+    console.log(`\n\n\n\n\nReview Begun! Running on port ${process.env.REVIEW_PORT || 5876}...\n\n\n\n\n`);
+    
     main();
-
-    console.log(`Review Begun! Running on port ${process.env.REVIEW_PORT || 5876}...`);
 } catch (error) {
     console.error(error);
 
