2720ee43396ee5f86b3821321632a40fc1a1d1ca
techpixel <68567672+techpixel@users.noreply.github.com>
2024-07-17 11:01:41 -0400

    mb i made a typo

--- src/lib/airtable.ts
+++ src/lib/airtable.ts
@@ -7,7 +7,7 @@ import Airtable from "airtable";
 import { emitter } from './emitter.js';
 
 Airtable.configure({
-    apiKey: process.env.AIRTABLE_API_KEY
+    apiKey: process.env.AIRTABLE_TOKEN
 });
 
 if (!process.env.AIRTABLE_BASE) { throw new Error("No Airtable base provided"); }
