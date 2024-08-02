79b916b2eaed4bb50e4cc2e26aa009daaaac916e
techpixel <68567672+techpixel@users.noreply.github.com>
2024-08-02 15:13:05 -0400

    add bottlenecks

--- src/extensions/arcade/watchers/airtable_poll.ts
+++ src/extensions/arcade/watchers/airtable_poll.ts
@@ -5,54 +5,44 @@ import { Environment } from "../../../lib/constants.js";
 import { Slack } from "../../../lib/bolt.js";
 import { t } from "../../../lib/templates.js";
 
+import Bottleneck from "bottleneck";
+
+// Span the requests out a bit, maybe like 1 per 15 seconds
+const limiter = new Bottleneck({
+    minTime: 15 * 1000,
+    maxConcurrent: 1
+});
+
 async function pollSyncSessions() {
-    const airtableSessions = await AirtableAPI.Session.filter(`DATETIME_DIFF(NOW(), LAST_MODIFIED_TIME(), 'days') < 1`).catch(console.error);
+    try {
+        const airtableSessions = await AirtableAPI.Session.filter(`DATETIME_DIFF(NOW(), LAST_MODIFIED_TIME(), 'days') < 1`).catch(console.error);
 
-    if (!airtableSessions) { throw new Error('Failed to fetch sessions from Airtable'); }
+        if (!airtableSessions) { throw new Error('Failed to fetch sessions from Airtable'); }
 
-    for (const airtableSession of airtableSessions) {
-        const record = airtableSession.id;
+        for (const airtableSession of airtableSessions) {
+            const record = airtableSession.id;
 
-        console.log(`[Airtable Poll] Received session ${record} from Airtable`);
-    
-        const session = await prisma.session.findFirstOrThrow({
-            where: {
-                metadata: {
-                    path: ["airtable", "id"],
-                    equals: record
+            console.log(`[Airtable Poll] Received session ${record} from Airtable`);
+        
+            const session = await prisma.session.findFirstOrThrow({
+                where: {
+                    metadata: {
+                        path: ["airtable", "id"],
+                        equals: record
+                    }
                 }
-            }
-        });
-
-        if (airtableSession.fields["Status"] === session.metadata.airtable?.status) {
-            console.log(`[Airtable Poll] Status of session ${session.messageTs} is up to date`);
-            continue;
-        }
-
-        session.metadata.airtable = {
-            id: record,
-            status: airtableSession.fields["Status"],
-            reason: airtableSession.fields["Reason"]
-        };
-
-        await prisma.session.update({
-            where: {
-                id: session.id
-            },
-            data: {
-                metadata: session.metadata
-            }
-        });
-
-        console.log(`[Airtable Poll] Status of session ${session.messageTs} updated to ${airtableSession.fields["Status"]}`);
-
-        // Check if it was approved & has a scrapbook
-        if (airtableSession.fields["Status"] === "Approved" && airtableSession.fields["Scrapbook"].length > 0) {
-            await AirtableAPI.Session.update(airtableSession.id, {
-                "Status": "Banked"
             });
 
-            // session.metadata.airtable!.status = "Banked";
+            if (airtableSession.fields["Status"] === session.metadata.airtable?.status) {
+                console.log(`[Airtable Poll] Status of session ${session.messageTs} is up to date`);
+                continue;
+            }
+
+            session.metadata.airtable = {
+                id: record,
+                status: airtableSession.fields["Status"],
+                reason: airtableSession.fields["Reason"]
+            };
 
             await prisma.session.update({
                 where: {
@@ -63,56 +53,78 @@ async function pollSyncSessions() {
                 }
             });
 
-            console.log(`[Airtable Poll] Queued session ${session.messageTs} for banking`);
-        }
+            console.log(`[Airtable Poll] Status of session ${session.messageTs} updated to ${airtableSession.fields["Status"]}`);
 
-        const slackUser = await prisma.slackUser.findUniqueOrThrow({
-            where: {
-                userId: session.userId
-            },
-            select: {
-                slackId: true
+            // Check if it was approved & has a scrapbook
+            if (airtableSession.fields["Status"] === "Approved" && airtableSession.fields["Scrapbook"].length > 0) {
+                await AirtableAPI.Session.update(airtableSession.id, {
+                    "Status": "Banked"
+                });
+
+                // session.metadata.airtable!.status = "Banked";
+
+                await prisma.session.update({
+                    where: {
+                        id: session.id
+                    },
+                    data: {
+                        metadata: session.metadata
+                    }
+                });
+
+                console.log(`[Airtable Poll] Queued session ${session.messageTs} for banking`);
             }
-        });
 
-        const permalink = await Slack.chat.getPermalink({
-            channel: Environment.MAIN_CHANNEL,
-            message_ts: session.messageTs
-        });
+            const slackUser = await prisma.slackUser.findUniqueOrThrow({
+                where: {
+                    userId: session.userId
+                },
+                select: {
+                    slackId: true
+                }
+            });
 
-        if (!permalink || !permalink.permalink) { throw new Error(`No permalink found for ${session.messageTs}`); }
+            const permalink = await limiter.schedule(() => Slack.chat.getPermalink({
+                channel: Environment.MAIN_CHANNEL,
+                message_ts: session.messageTs
+            }));
 
-        // Send a message in that thread saying it was updated
-        if (session.metadata.airtable!.status === "Approved") {
-            await Slack.chat.postMessage({
-                channel: Environment.MAIN_CHANNEL,
-                thread_ts: session.messageTs,
-                text: t('airtable.approved', {
-                    slackId: slackUser.slackId,
-                    minutes: airtableSession.fields['Approved Minutes']
-                })
-            });
-        } else if (
-            session.metadata.airtable!.status === "Rejected"
-        ) {
-            await Slack.chat.postMessage({
-                channel: Environment.MAIN_CHANNEL,
-                thread_ts: session.messageTs,
-                text: t('airtable.rejected', {
-                    slackId: slackUser.slackId
-                })
-            });
-        } else if (
-            session.metadata.airtable!.status === "Rejected Locked"
-        ) {
-            await Slack.chat.postMessage({
-                channel: Environment.MAIN_CHANNEL,
-                thread_ts: session.messageTs,
-                text: t('airtable.rejectedlocked', {
-                    slackId: slackUser.slackId
-                })
-            });
+            if (!permalink || !permalink.permalink) { throw new Error(`No permalink found for ${session.messageTs}`); }
+
+            // Send a message in that thread saying it was updated
+            if (session.metadata.airtable!.status === "Approved") {
+                limiter.schedule(() => Slack.chat.postMessage({
+                    channel: Environment.MAIN_CHANNEL,
+                    thread_ts: session.messageTs,
+                    text: t('airtable.approved', {
+                        slackId: slackUser.slackId,
+                        minutes: airtableSession.fields['Approved Minutes']
+                    })
+                }));
+            } else if (
+                session.metadata.airtable!.status === "Rejected"
+            ) {
+                limiter.schedule(() => Slack.chat.postMessage({
+                    channel: Environment.MAIN_CHANNEL,
+                    thread_ts: session.messageTs,
+                    text: t('airtable.rejected', {
+                        slackId: slackUser.slackId
+                    })
+                }));
+            } else if (
+                session.metadata.airtable!.status === "Rejected Locked"
+            ) {
+                limiter.schedule(() => Slack.chat.postMessage({
+                    channel: Environment.MAIN_CHANNEL,
+                    thread_ts: session.messageTs,
+                    text: t('airtable.rejectedlocked', {
+                        slackId: slackUser.slackId
+                    })
+                }));
+            }
         }
+    } catch (error) {
+        console.error(`[Airtable Poll] ${error}`);
     }
 }
 
@@ -122,4 +134,4 @@ async function main() {
     main();
 }
 
-pollSyncSessions();
+main();
