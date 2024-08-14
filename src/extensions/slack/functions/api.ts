860253755e7be74136025968e8fb0c76cc429f9e
techpixel <68567672+techpixel@users.noreply.github.com>
2024-08-13 21:13:19 -0500

    wrap in exception block

--- src/extensions/slack/functions/api.ts
+++ src/extensions/slack/functions/api.ts
@@ -7,54 +7,58 @@ import { API } from "../views/api.js";
 import { scryptSync } from "crypto";
 
 Slack.command(Commands.API, async ({ body, respond }) => {
-    if (!AirtableAPI.User.isAuthorized(body.user_id)) {
-        await respond({
-            response_type: 'ephemeral',
-            text: t('error.not_authorized'),
-        });
+    try {
+        if (!AirtableAPI.User.isAuthorized(body.user_id)) {
+            await respond({
+                response_type: 'ephemeral',
+                text: t('error.not_authorized'),
+            });
 
-        return;
-    }
-
-    const slackUser = await prisma.slackUser.findUnique({
-        where: {
-            slackId: body.user_id,
-        },
-    });
-
-    if (!slackUser) {
-        await respond({
-            response_type: 'ephemeral',
-            text: t('error.not_a_user'),
-        });
-        
-        return;
-    }
-
-    // const authorized = await AirtableAPI.User.isAuthorized(slackUser.slackId);
-
-    // if (!authorized) {
-    //     await respond({
-    //         response_type: 'ephemeral',
-    //         text: t('error.not_authorized'),
-    //     });
-        
-    //     return;
-    // }
-
-    const apiKey = crypto.randomUUID();
-
-   await prisma.user.update({
-        where: {
-            id: slackUser.userId,
-        },
-        data: {
-            apiKey: scryptSync(apiKey, 'salt', 64).toString('hex'),
+            return;
         }
-    });
 
-    await Slack.views.open({
-        trigger_id: body.trigger_id,
-        view: API.api(apiKey),
-    });
+        const slackUser = await prisma.slackUser.findUnique({
+            where: {
+                slackId: body.user_id,
+            },
+        });
+
+        if (!slackUser) {
+            await respond({
+                response_type: 'ephemeral',
+                text: t('error.not_a_user'),
+            });
+            
+            return;
+        }
+
+        // const authorized = await AirtableAPI.User.isAuthorized(slackUser.slackId);
+
+        // if (!authorized) {
+        //     await respond({
+        //         response_type: 'ephemeral',
+        //         text: t('error.not_authorized'),
+        //     });
+            
+        //     return;
+        // }
+
+        const apiKey = crypto.randomUUID();
+
+        await prisma.user.update({
+            where: {
+                id: slackUser.userId,
+            },
+            data: {
+                apiKey: scryptSync(apiKey, 'salt', 64).toString('hex'),
+            }
+        });
+
+        await Slack.views.open({
+            trigger_id: body.trigger_id,
+            view: API.api(apiKey),
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
