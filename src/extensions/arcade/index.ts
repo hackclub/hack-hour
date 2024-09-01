cf2493616866300469b8c737510d85ad9585597b
techpixel <68567672+techpixel@users.noreply.github.com>
2024-08-31 23:09:39 -0500

    re-add shop through qshop

--- src/extensions/arcade/index.ts
+++ src/extensions/arcade/index.ts
@@ -12,7 +12,48 @@ import "./slack/walkthrough.js";
 import "./slack/showcase.js";
 
 import { emitter } from "../../lib/emitter.js";
+import { Slack } from "../../lib/bolt.js";
+import { prisma } from "../../lib/prisma.js";
+import { t } from "../../lib/templates.js";
+import { Environment } from "../../lib/constants.js";
 
 emitter.on('init', () => {
     console.log('[Arcade] 🕹️ Arcade Initialized!')
 })
+
+
+Slack.command('/shop', async ({ command }) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            slackUser: {
+                slackId: command.user_id
+            }
+        }
+    });
+
+    if (!user) {
+        await Slack.chat.postMessage({
+            channel: command.channel_id,
+            text: t('error.first_time')
+        });
+
+        return;
+    }
+
+    const recordId = user.metadata.airtable?.id;
+
+    if (!recordId) {
+        await Slack.chat.postMessage({
+            channel: command.channel_id,
+            text: t('error.first_time')
+        });
+
+        return;
+    }
+
+    Slack.chat.postEphemeral({
+        channel: command.channel_id,
+        user: command.user_id,
+        text: `<${Environment.SHOP_URL}/arcade/${recordId}/shop/|Open the shop!>`
+    });
+});
