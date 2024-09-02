0aad12905ba8b9b969f0685d3b9b684d1be78cf1
techpixel <68567672+techpixel@users.noreply.github.com>
2024-09-02 18:36:12 -0500

    see if this fixes the shop issue

--- src/extensions/slack/index.ts
+++ src/extensions/slack/index.ts
@@ -822,3 +822,43 @@ emitter.on('debug', async (message) => {
     }
 });
 
+
+Slack.command('/shop', async ({ command }) => {
+try {
+    const user = await prisma.user.findFirst({
+        where: {
+            slackUser: {
+                slackId: command.user_id
+            }
+        }
+    });
+
+    if (!user) {
+        await Slack.chat.postMessage({
+            channel: command.channel_id,
+            text: t('error.first_time')
+        });
+
+        return;
+    }
+
+    const recordId = user.metadata.airtable?.id;
+
+    if (!recordId) {
+        await Slack.chat.postMessage({
+            channel: command.channel_id,
+            text: t('error.first_time')
+        });
+
+        return;
+    }
+
+    Slack.chat.postEphemeral({
+        channel: command.channel_id,
+        user: command.user_id,
+        text: `<${Environment.SHOP_URL}/arcade/${recordId}/shop/|Open the shop!>`
+    });
+} catch (e) {
+    console.error(e);
+}
+});
